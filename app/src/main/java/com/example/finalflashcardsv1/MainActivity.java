b7e0bd7d963a1fe67ebbbab4b579a52ceb34e939
Jack Watai <jack.watai4444@gmail.com>
2021-04-07 18:26:00 +0800

    added topic 1

--- app/src/main/java/com/example/finalflashcardsv1/MainActivity.java
+++ app/src/main/java/com/example/finalflashcardsv1/MainActivity.java
@@ -8,53 +8,42 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button button_topic1;
-    Button button_topic2;
-    Button button_topic3;
+    Button buttontopic1;
+    Button buttontopic2;
+    Button buttontopic3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button_topic1 =(Button) findViewById(R.id.button_topic1);
-        button_topic1.setOnClickListener(new View.OnClickListener() {
+        buttontopic1 = (Button) findViewById(R.id.button_topic1);
+        buttontopic1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openTopic1();
+                Intent intent = new Intent(MainActivity.this, Topic1.class);
+                startActivity(intent);
             }
         });
 
-        button_topic2 =(Button) findViewById(R.id.button_topic2);
-        button_topic2.setOnClickListener(new View.OnClickListener() {
+        buttontopic2 = (Button) findViewById(R.id.button_topic2);
+        buttontopic2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openTopic2();
+                Intent intent = new Intent(MainActivity.this, Topic2.class);
+                startActivity(intent);
             }
         });
 
-        button_topic3 =(Button) findViewById(R.id.button_topic3);
-        button_topic3.setOnClickListener(new View.OnClickListener() {
+        buttontopic3 = (Button) findViewById(R.id.button_topic3);
+        buttontopic3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openTopic3();
+                Intent intent = new Intent(MainActivity.this, Topic3.class);
+                startActivity(intent);
             }
         });
 
-    }
-    public void openTopic1(){
-        Intent intent = new Intent(this,Topic1.class);
-        startActivity(intent);
-    }
 
-    public void openTopic2(){
-        Intent intent = new Intent(this,Topic2.class);
-        startActivity(intent);
     }
-
-    public void openTopic3(){
-        Intent intent = new Intent(this,Topic3.class);
-        startActivity(intent);
-    }
-
 }
